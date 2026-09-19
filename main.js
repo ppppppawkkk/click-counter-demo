64fe17015cd73c1d9139c73cf6948f18f97f9f75
Lune <chakkirdsinpea@gmail.com>
2026-09-19 21:46:51 +0700

    67

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let count = 0;
 
 const countText = document.querySelector("#count");
 const button = document.querySelector("#click-button");
+const monkeyImg = document.querySelector("#monkey-img");
 
 button.addEventListener("click", () => {
     count = count + 1;
@@ -11,6 +12,13 @@ button.addEventListener("click", () => {
         button.textContent = "คลิกทำไหมมมมมมมมมมมมม";
     } else if (count === 2) {
         button.textContent = "ยิ่งคลิกยิ่งเอ๋อนะ";
+    } else if (count === 67) {
+        button.textContent = "บอกแล้วว่าอย่ากดถึง 67!";
+        if (monkeyImg) {
+            monkeyImg.style.display = "inline-block"; // แสดงรูปลิง
+        }
+    } else if (count > 67) {
+        button.textContent = `เกิน 67 มา ${count - 67} ครั้งแล้ว พอได้แล้ว!`;
     } else {
         button.textContent = `บอกให้หยุดไง! (กดไป ${count} ครั้งแล้วนะ ถ้ากดถึง 67 มีอะไรให้ดู)`;
     }
