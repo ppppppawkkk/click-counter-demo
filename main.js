7461049c71ca0c865b05b6179b7d8102865e76fa
Lune <chakkirdsinpea@gmail.com>
2026-09-19 21:36:11 +0700

    Upadat button text on click

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let count = 0;
 
-const countText = document.querySelector("#count")
-const button = document.querySelector("#click-button")
+const countText = document.querySelector("#count");
+const button = document.querySelector("#click-button");
 
 button.addEventListener("click", () => {
     count = count + 1;
     countText.textContent = count;
 
+    if (count === 1) {
+        button.textContent = "คลิกทำไหมมมมมมมมมมมมม";
+    } else if (count === 2) {
+        button.textContent = "หยุดดดดดดดดดดดดด";
+    } else {
+        button.textContent = `บอกให้หยุดไง! (กดไป ${count} ครั้งแล้วนะ)`;
+    }
+
     console.log("จำนวนคลิก:", count);
-})
+});
